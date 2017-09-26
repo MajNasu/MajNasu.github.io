f2481c6d8c8ebcf502d301a218301aba503222d5
Susan <sungjamesjoo@gmail.com>
2017-09-26 16:57:38 -0400

    Added welcome

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(()=>{
+  count = 0;
+  welcomeArray = ["HELLO!", "안녕하세요!"];
+  setInterval(function(){
+    count++;
+    $('#welcome').fadeOut(400, function(){
+      $(this).text(welcomeArray[count % welcomeArray.length]).fadeIn(400);
+    });
+  }, 4000);
+});
